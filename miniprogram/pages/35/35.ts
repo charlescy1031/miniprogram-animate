9e9e5d007c89d1c174e480db930a472f58bc5229
cuiyuan <charlescy1031@icloud.com>
2023-11-03 17:26:00 +0800

    update

--- miniprogram/pages/35/35.ts
+++ miniprogram/pages/35/35.ts
@@ -2,18 +2,17 @@
 
 Page({
   data: {
-    storeTopB: 250,
-    storeTopC: 400,
+    isAOpen: true,
+    isBOpen: false,
+    isCOpen: false,
     cardList: [{
-      top: "top:0;",
       cardSrc: "/image/35/送礼专区-机制_01.png", cardTitle: "语音卡片", cardDesc: "（温馨提示：如不定制礼品卡，将默认官方礼品卡随之赠送）"
     },
     {
-      top: "top:250px;",
       cardSrc: "/image/35/送礼专区-机制_02.png", cardTitle: "礼盒包装", cardDesc: ""
     },
     {
-      top: "top:400px;", cardSrc: "/image/35/送礼专区-机制_03.png", cardTitle: "专属赠品", cardDesc: ""
+      cardSrc: "/image/35/送礼专区-机制_03.png", cardTitle: "专属赠品", cardDesc: ""
     }],
     showNav: true
   },
@@ -39,80 +38,124 @@ Page({
   // 事件处理函数
   clickfn(e: any) {
     var index = e.currentTarget.dataset.index;
+    console.log(index)
     let animationA = wx.createAnimation({
-      duration: 200,
-      timingFunction: 'linear',
+      duration: 1500,
+      timingFunction: 'ease',
       delay: 0,
       transformOrigin: '72% 80%'
     })
-
-
-    if (index === 0) {
-      // 数据复位
-      animationA.opacity(1).step({ duration: 200 })
-      let tempA = 0;
-      let posSetinterval = setInterval(() => {
-        if (tempA >= 100) {
-          clearInterval(posSetinterval)
-          this.data.storeTopB = 250;
-          this.data.storeTopC = 400;
-          this.setData({
-            ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
-            ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
-          })
-        } else {
-          this.setData({
-            ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
-            ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
-          })
-          this.data.storeTopB += 4
-          this.data.storeTopC += 4
-          tempA += 4
-        }
-      }, 1)
-
-
-     
-
-    } else if (index === 1) {
-      animationA.opacity(0).step({ duration: 200 })
-      let posSetinterval = setInterval(() => {
-        if (this.data.storeTopB <= 150) {
-          clearInterval(posSetinterval)
-          this.data.storeTopB = 150;
-          this.setData({
-            ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
-          })
-        } else {
-          this.setData({
-            ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
-          })
-          this.data.storeTopB -= 4
-        }
-      }, 1)
-
-    } else {
-      animationA.opacity(0).step({ duration: 200 })
-
-      let posSetinterval = setInterval(() => {
-        if (this.data.storeTopC <= 300) {
-          clearInterval(posSetinterval)
-          this.data.storeTopC = 300;
-          this.setData({
-            ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
-          })
-        } else {
-          this.setData({
-            ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
-          })
-          this.data.storeTopC -= 4
-        }
-      }, 1)
-    }
-    // 将动画导出到页面
-    this.setData({
-      animationData: animationA.export()
+    let animationB = wx.createAnimation({
+      duration: 1500,
+      timingFunction: 'ease',
+      delay: 0,
+      transformOrigin: '72% 80%'
     })
+    let animationC = wx.createAnimation({
+      duration: 1500,
+      timingFunction: 'ease',
+      delay: 0,
+    })
+    if (index === 0 && this.data.isCOpen) {
+      animationA.opacity(1).step({ duration: 1000 })
+      animationB.top(250).step({ duration: 1000 })
+      animationC.top(400).step({ duration: 1000 })
+      this.setData({ isAOpen: true, isBOpen: false, isCOpen: false })
+    }
+
+
+    if (index === 1 && this.data.isAOpen) {
+      animationA.opacity(0).step({ duration: 1000 })
+      animationB.top(150).step({ duration: 1000 })
+      this.setData({ isAOpen: false, isBOpen: true, isCOpen: false })
+    } else if (index === 1 && !this.data.isAOpen) {
+      animationA.opacity(1).step({ duration: 1000 })
+      animationB.top(250).step({ duration: 1000 })
+      animationC.top(400).step({ duration: 1000 })
+      this.setData({ isAOpen: true, isBOpen: false, isCOpen: false })
+    }else{
+
+    }
+    if (index === 2 && !this.data.isCOpen) {
+      animationA.opacity(0).step({ duration: 1000 })
+      animationB.top(150).step({ duration: 1000 })
+      animationC.top(300).step({ duration: 1000 })
+      this.setData({ isAOpen: false, isBOpen: false, isCOpen: true })
+    } else if (index === 2 && this.data.isCOpen) {
+      animationC.top(400).step({ duration: 1000 })
+      this.setData({ isAOpen: false, isBOpen: true, isCOpen: false })
+    }
+
+
+    this.setData({
+      animationData1: animationA.export(),
+      animationData2: animationB.export(),
+      animationData3: animationC.export()
+    })
+    // if (index === 0) {
+    //   // 数据复位
+    //   animationA.opacity(1).step({ duration: 200 })
+    //   let tempA = 0;
+    //   let posSetinterval = setInterval(() => {
+    //     if (tempA >= 100) {
+    //       clearInterval(posSetinterval)
+    //       this.data.storeTopB = 250;
+    //       this.data.storeTopC = 400;
+    //       this.setData({
+    //         ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
+    //         ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
+    //       })
+    //     } else {
+    //       this.setData({
+    //         ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
+    //         ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
+    //       })
+    //       this.data.storeTopB += 4
+    //       this.data.storeTopC += 4
+    //       tempA += 4
+    //     }
+    //   }, 1)
+
+
+
+
+    // } else if (index === 1) {
+    //   animationA.opacity(0).step({ duration: 200 })
+    //   let posSetinterval = setInterval(() => {
+    //     if (this.data.storeTopB <= 150) {
+    //       clearInterval(posSetinterval)
+    //       this.data.storeTopB = 150;
+    //       this.setData({
+    //         ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
+    //       })
+    //     } else {
+    //       this.setData({
+    //         ['cardList[1].top']: `top:${this.data.storeTopB}px;`,
+    //       })
+    //       this.data.storeTopB -= 4
+    //     }
+    //   }, 1)
+
+    // } else {
+    //   animationA.opacity(0).step({ duration: 200 })
+
+    //   let posSetinterval = setInterval(() => {
+    //     if (this.data.storeTopC <= 300) {
+    //       clearInterval(posSetinterval)
+    //       this.data.storeTopC = 300;
+    //       this.setData({
+    //         ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
+    //       })
+    //     } else {
+    //       this.setData({
+    //         ['cardList[2].top']: `top:${this.data.storeTopC}px;`,
+    //       })
+    //       this.data.storeTopC -= 4
+    //     }
+    //   }, 1)
+    // }
+    // 将动画导出到页面
+
   }
 })
 
